f5d02275f7ca8f9ae9149adc835a05c375d142cc
Éderson <edersonvidal137@gmail.com>
2020-06-27 21:35:40 -0300

    Imagens importadas, erro na troca de objetos

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 
 pygame.init()
 
-############# Variáveis Gerais #############s
+############# Variáveis Gerais #############
 larguraTela = 800
 alturaTela = 600
 gamedisplay = pygame.display.set_mode((larguraTela,alturaTela))
@@ -14,22 +14,62 @@ black = (0,0,0)
 white = (255,255,255)
 gray =  (100,100,100)
 
-ironManImg = pygame.image.load('assets/ironLarge.png')
-misselImg = pygame.image.load('assets/missile.png')
-fundo = pygame.image.load('assets/sky.png')
-explo_sound = pygame.mixer.Sound('assets/explosao.wav')
-missel_sound = pygame.mixer.Sound('assets/missile.wav')
+## Lixo movimentavel
+obj_metal = pygame.image.load('assets/metal.png')
+obj_vidro = pygame.image.load('assets/vidro.png')
+obj_papel = pygame.image.load('assets/papel.png')
+obj_plast = pygame.image.load('assets/plastico.png')
+
+#Latas de lixo
+vidro_ama = pygame.image.load('assets/vidro-ama.png')
+vidro_verd = pygame.image.load('assets/vidro-verde.png')
+vidro_verm = pygame.image.load('assets/vidro-verm.png')
+vidro_azul = pygame.image.load('assets/vidro-azul.png')
+
+papel_ama = pygame.image.load('assets/papel-ama.png')
+papel_verd = pygame.image.load('assets/vidro-verde.png')
+papel_verm = pygame.image.load('assets/papel-verm.png')
+papel_azul = pygame.image.load('assets/papel-azul.png')
+
+plast_ama = pygame.image.load('assets/plastico-ama.png')
+plast_verd = pygame.image.load('assets/vidro-verde.png')
+plast_verm = pygame.image.load('assets/plastico-verm.png')
+plast_azul = pygame.image.load('assets/plastico-azul.png')
+
+metal_ama = pygame.image.load('assets/metal-ama.png')
+metal_verd = pygame.image.load('assets/vidro-verde.png')
+metal_verm = pygame.image.load('assets/metal-verm.png')
+metal_azul = pygame.image.load('assets/metal-azul.png')
+
+##fundo
+fundo = pygame.image.load('assets/fundo.png')
+
 
 
 ############# Funções Gerais #############
+
+def define_tam(objeto):
+    if objeto == obj_vidro:
+        larg_obj = 30
+        alt_obj = 116
+    elif objeto == obj_metal:
+        larg_obj = 56
+        alt_obj = 120
+    elif objeto == obj_papel:
+        larg_obj = 100
+        alt_obj = 122
+    elif objeto == obj_plast:
+        larg_obj = 129
+        alt_obj = 103
+
 def mostraFundo():
     gamedisplay.blit(fundo, (0,0) )
 
-def mostraIron( x, y ):
-    gamedisplay.blit(ironManImg, (x,y) )
+def mostra_obj(objeto, x, y):
+    gamedisplay.blit(objeto, (x, y))
 
-def mostraMissel( x, y ):
-    gamedisplay.blit(misselImg, (x,y) )
+def muda_obj():
+    return random.choice([obj_metal, obj_papel, obj_plast, obj_vidro])
 
 def text_objects(text, font):
     textSurface = font.render(text, True, black)
@@ -45,14 +85,17 @@ def mostra_mensagem(text):
     game_loop()
 
 
-def placar(contador=0):
+def reciclados(contador=0):
     fonte = pygame.font.SysFont(None, 25)
-    text = fonte.render('Desvios: '+ str(contador), True, white)
+    text = fonte.render('Reciclados: '+ str(contador), True, white)
     gamedisplay.blit(text, (10, 30))
 
-def morte_iron():
-    pygame.mixer.Sound.play(explo_sound)
-    pygame.mixer.music.stop()
+def desperdicados(contador=0):
+    fonte = pygame.font.SysFont(None, 25)
+    text = fonte.render('Desperdiçados: '+ str(contador), True, white)
+    gamedisplay.blit(text, (760, 770))
+
+def game_over():
     mostra_mensagem('Você morreu!')
 
 
@@ -61,70 +104,103 @@ def morte_iron():
 ############ Looping do Jogo
 
 def game_loop():
+    '''
     pygame.mixer.music.set_volume(0.01)
     pygame.mixer.music.load('assets/ironsound.mp3')
     pygame.mixer.music.play(-1)
+    '''
 
-    ironPosicaoX = 330
-    ironPosicaoY = 470
+    pos_objX = 400
+    pos_objY = 300
     movi_x = 0
-    altura_iron = 110
-    largura_iron = 120
+    movi_y = 0
+    larg_obj = 0
+    alt_obj = 0
 
+    pos_lata1X = 100
+    pos_lata1Y = 100
+    larg_lata1 = 1
+    alt_lata1 = 1
 
-    missel_x = random.randrange(0, larguraTela)
-    missel_y = 0 - 250
-    altura_missel = 250
-    largura_missel = 50
-    velo_missel = 7
+    pos_lata2X = 600
+    pos_lata2Y = 100
+    larg_lata2 = 110
+    alt_lata2 = 120
 
+    pos_lata3X = 100
+    pos_lata3Y = 600
+    larg_lata3 = 110
+    alt_lata3 = 120
+
+    pos_lata4X = 600
+    pos_lata4Y = 600
+    larg_lata4 = 110
+    alt_lata4 = 120
+
+    troca_obj = 0
+    obj_mostrado = random.choice([obj_metal, obj_papel, obj_plast, obj_vidro])
+    
     cont = 0
     
     while True:
         # event.get() devolve uma lista de eventos que estão acontecendo
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 # quit() é comando native terminar o programa
                 quit()
-            
+
+            if troca_obj == 0:
+                obj_mostrado = random.choice([obj_metal, obj_papel, obj_plast, obj_vidro])
+                define_tam(obj_mostrado)
+                troca_obj += 1
+            else:
+                pass
+
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     movi_x = -5
                 if event.key == pygame.K_RIGHT:
                     movi_x = 5
+                if event.key == pygame.K_DOWN:
+                    movi_y = 5
+                if event.key == pygame.K_UP:
+                    movi_y = -5
                 if event.key == pygame.K_ESCAPE:
                     pygame.quit()
+                    
+            if pos_objX > larguraTela - larg_obj or pos_objX < 0:
+                 movi_x = 0
+                 game_loop()
+
+            if pos_objY > alturaTela - alt_obj or pos_objY < 0:
+                movi_x = 0
+                troca_obj = 0
+                break
+                
 
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                     movi_x = 0
+                if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                    movi_y = 0
 
-            if ironPosicaoX > larguraTela - largura_iron or ironPosicaoX < 0:
-                movi_x = 0
-                morte_iron()
+            
 
-            if missel_y > alturaTela:
-                pygame.mixer.music.set_volume(0.01)
-                pygame.mixer.Sound.play(missel_sound)
-                missel_y = 0 - altura_missel
-                missel_x = random.randrange(0, larguraTela)
-                velo_missel += 1
-                cont += 1
-
+            '''
             if ironPosicaoY < missel_y + altura_missel:
                 if ironPosicaoX < missel_x and ironPosicaoX + largura_iron > missel_x or missel_x + largura_missel > ironPosicaoX and missel_x + largura_missel < ironPosicaoX + largura_iron:
                     morte_iron()
+            '''
 
                 
 
-        ironPosicaoX += movi_x
-        missel_y += velo_missel        
+        pos_objX += movi_x
+        pos_objY += movi_y      
 
         mostraFundo()
-        placar(cont)
-        mostraIron(ironPosicaoX,ironPosicaoY)
-        mostraMissel(missel_x, missel_y)
+        mostra_obj(obj_mostrado, pos_objX, pos_objY)
         pygame.display.update()
         clock.tick(60)
 
