d10b2f0f612ad4653f4c4fc978c6cccd10e0da9e
Éderson <edersonvidal137@gmail.com>
2020-06-28 10:43:24 -0300

    Exibicao das latas e reconhecimento de colisao

--- main.py
+++ main.py
@@ -27,17 +27,17 @@ vidro_verm = pygame.image.load('assets/vidro-verm.png')
 vidro_azul = pygame.image.load('assets/vidro-azul.png')
 
 papel_ama = pygame.image.load('assets/papel-ama.png')
-papel_verd = pygame.image.load('assets/vidro-verde.png')
+papel_verd = pygame.image.load('assets/papel-verde.png')
 papel_verm = pygame.image.load('assets/papel-verm.png')
 papel_azul = pygame.image.load('assets/papel-azul.png')
 
 plast_ama = pygame.image.load('assets/plastico-ama.png')
-plast_verd = pygame.image.load('assets/vidro-verde.png')
+plast_verd = pygame.image.load('assets/plastico-verde.png')
 plast_verm = pygame.image.load('assets/plastico-verm.png')
 plast_azul = pygame.image.load('assets/plastico-azul.png')
 
 metal_ama = pygame.image.load('assets/metal-ama.png')
-metal_verd = pygame.image.load('assets/vidro-verde.png')
+metal_verd = pygame.image.load('assets/metal-verde.png')
 metal_verm = pygame.image.load('assets/metal-verm.png')
 metal_azul = pygame.image.load('assets/metal-azul.png')
 
@@ -62,14 +62,30 @@ def define_tam(objeto):
         larg_obj = 129
         alt_obj = 103
 
+
 def mostraFundo():
     gamedisplay.blit(fundo, (0,0) )
 
 def mostra_obj(objeto, x, y):
     gamedisplay.blit(objeto, (x, y))
 
-def muda_obj():
+def mostra_lata1(lata1, x, y):
+    gamedisplay.blit(lata1, (x, y))
+
+def mostra_lata2(lata2, x, y):
+    gamedisplay.blit(lata2, (x, y))
+
+def mostra_lata3(lata3, x, y):
+    gamedisplay.blit(lata3, (x, y))
+
+def mostra_lata4(lata4, x, y):
+    gamedisplay.blit(lata4, (x, y))
+
+def novo_obj():
     return random.choice([obj_metal, obj_papel, obj_plast, obj_vidro])
+
+def nova_lata(arg1, arg2, arg3, arg4):
+    return random.choice([arg1, arg2, arg3, arg4])
 
 def text_objects(text, font):
     textSurface = font.render(text, True, black)
@@ -110,34 +126,39 @@ def game_loop():
     pygame.mixer.music.play(-1)
     '''
 
-    pos_objX = 400
-    pos_objY = 300
+    pos_objX = 375
+    pos_objY = 275
     movi_x = 0
     movi_y = 0
     larg_obj = 0
     alt_obj = 0
 
-    pos_lata1X = 100
-    pos_lata1Y = 100
-    larg_lata1 = 1
-    alt_lata1 = 1
+    pos_lata1X = 50
+    pos_lata1Y = 50
+    larg_lata1 = 131
+    alt_lata1 = 210
 
     pos_lata2X = 600
-    pos_lata2Y = 100
-    larg_lata2 = 110
-    alt_lata2 = 120
+    pos_lata2Y = 50
+    larg_lata2 = 131
+    alt_lata2 = 210
 
-    pos_lata3X = 100
-    pos_lata3Y = 600
-    larg_lata3 = 110
-    alt_lata3 = 120
+    pos_lata3X = 50
+    pos_lata3Y = 350
+    larg_lata3 = 131
+    alt_lata3 = 210
 
     pos_lata4X = 600
-    pos_lata4Y = 600
-    larg_lata4 = 110
-    alt_lata4 = 120
+    pos_lata4Y = 350
+    larg_lata4 = 131
+    alt_lata4 = 210
 
-    obj_mostrado = random.choice([obj_metal, obj_papel, obj_plast, obj_vidro])
+    obj_mostrado = novo_obj()
+
+    lata1 = nova_lata(vidro_ama, vidro_azul, vidro_verd, vidro_verm)
+    lata2 = nova_lata(metal_ama, metal_azul, metal_verd, metal_verm)
+    lata3 = nova_lata(plast_ama, plast_azul, plast_verd, plast_verm)
+    lata4 = nova_lata(papel_ama, papel_azul, papel_verd, papel_verm)
 
     if obj_mostrado == obj_vidro:
         larg_obj = 30
@@ -152,7 +173,8 @@ def game_loop():
         larg_obj = 129
         alt_obj = 103
 
-    cont = 0
+    recic = 0
+    desper = 0
     
     while True:
         # event.get() devolve uma lista de eventos que estão acontecendo
@@ -189,21 +211,69 @@ def game_loop():
                 if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                     movi_y = 0
 
+            '''
+            if ironPosicaoX < missel_x and ironPosicaoX + largura_iron > missel_x 
+            or missel_x + largura_missel > ironPosicaoX and missel_x + largura_missel < ironPosicaoX + largura_iron:
+            '''
+
+            if pos_objX  < pos_lata1X + larg_lata1 and pos_objY < pos_lata1Y + larg_lata1:
+                if obj_mostrado == obj_papel:
+                    desper +=1
+                elif obj_mostrado == obj_metal:
+                    desper += 1
+                elif obj_mostrado == obj_plast:
+                    desper += 1
+                elif obj_mostrado == obj_vidro:
+                    recic += 1  
+
+            if pos_objX + larg_obj  > pos_lata2X and pos_objY < pos_lata2Y + larg_lata2:
+                if obj_mostrado == obj_papel:
+                    desper +=1
+                elif obj_mostrado == obj_metal:
+                    recic += 1
+                elif obj_mostrado == obj_plast:
+                    desper += 1
+                elif obj_mostrado == obj_vidro:
+                    desper += 1  
+                
+
+            if pos_objX < pos_lata3X + larg_lata3 and pos_objY + alt_obj > pos_lata3Y:
+                if obj_mostrado == obj_papel:
+                    desper +=1
+                elif obj_mostrado == obj_metal:
+                    desper += 1
+                elif obj_mostrado == obj_plast:
+                    recic += 1
+                elif obj_mostrado == obj_vidro:
+                    desper += 1  
+                
+
+            if pos_objX + larg_obj > pos_lata4X and pos_objY + alt_obj > pos_lata4Y:
+                if obj_mostrado == obj_papel:
+                    desper +=1
+                elif obj_mostrado == obj_metal:
+                    desper += 1
+                elif obj_mostrado == obj_plast:
+                    desper += 1
+                elif obj_mostrado == obj_vidro:
+                    recic += 1  
+                
+
             
-
-            '''
-            if ironPosicaoY < missel_y + altura_missel:
-                if ironPosicaoX < missel_x and ironPosicaoX + largura_iron > missel_x or missel_x + largura_missel > ironPosicaoX and missel_x + largura_missel < ironPosicaoX + largura_iron:
-                    morte_iron()
-            '''
-
                 
 
         pos_objX += movi_x
         pos_objY += movi_y      
 
         mostraFundo()
+
+        mostra_lata1(lata1, pos_lata1X, pos_lata1Y)
+        mostra_lata2(lata2, pos_lata2X, pos_lata2Y)
+        mostra_lata3(lata3, pos_lata3X, pos_lata3Y)
+        mostra_lata4(lata4, pos_lata4X, pos_lata4Y)
+
         mostra_obj(obj_mostrado, pos_objX, pos_objY)
+
         pygame.display.update()
         clock.tick(60)
 
